a066aa28aa0c80218a09f518178aa62cf03977a9
hyoniii <phhphh841@hanmail.net>
2022-06-10 17:58:10 +0900

    feat : Create a vue project and install vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
+//default가 아닌 설정 항목들이 추가 되는
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: [
+    'vuetify'
+  ]
 })
