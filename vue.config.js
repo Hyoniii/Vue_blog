d896613cbf7204fcc889ee1353ec432abdb01d01
hyoniii <phhphh841@hanmail.net>
2022-06-11 15:50:55 +0900

    feat : added 3 webpack setting items

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  outputDir: 'dist',
+  publicPath: '/',
+  assetsDir: 'static'
 })
